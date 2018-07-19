ce193867679b4a8a873d7e894e5924c0ab39bc32
Tobias <githubst@gmail.com>
2018-07-19 20:48:20 +0200

    Refactor with Google rules ;

--- extractor.js
+++ extractor.js
@@ -2,30 +2,35 @@
  * Declare Variables
  */
 
-let yearsView = document.querySelector("#years"),
-  unemploymentView = document.querySelector("#percent"),
-  maxView = document.querySelector("#max"),
-  minView = document.querySelector("#min"),
-  averageView = document.querySelector("#average"),
-  cityView = document.querySelector("#listOfCity");
+let yearsView = document.querySelector('#years');
+
+let unemploymentView = document.querySelector('#percent');
+
+let maxView = document.querySelector('#max');
+
+let minView = document.querySelector('#min');
+
+let averageView = document.querySelector('#average');
+
+let cityView = document.querySelector('#listOfCity');
 
 /**
  * Creating Options for the Select
  */
 
-const cities = data.map(x => Object.keys(x.fields));
+const cities = data.map((x) => Object.keys(x.fields));
 
 for (let index = 0; index < cities[0].length; index++) {
-  let newOptions = document.createElement("option");
-  newOptions.text = cities[0][index];
+   let newOptions = document.createElement('option');
+   newOptions.text = cities[0][index];
 
-  if (
-    newOptions.text != "annee" &&
-    newOptions.text != "numero_du_trimestre" &&
-    newOptions.text != "ile_de_france"
-  ) {
-    cityView.add(newOptions);
-  }
+   if (
+      newOptions.text != 'annee' &&
+      newOptions.text != 'numero_du_trimestre' &&
+      newOptions.text != 'ile_de_france'
+   ) {
+      cityView.add(newOptions);
+   }
 }
 
 /**
@@ -33,19 +38,19 @@ for (let index = 0; index < cities[0].length; index++) {
  */
 
 let choiceSelected;
-cityView.addEventListener("change", e => {
-  choiceSelected = e.currentTarget.value;
+cityView.addEventListener('change', (e) => {
+   choiceSelected = e.currentTarget.value;
 
-  let allInfos = document.querySelectorAll(".hidden");
+   let allInfos = document.querySelectorAll('.hidden');
 
-  /* Displaying Infos */
-  allInfos.forEach(item => {
-    item.classList.remove("hidden");
-  });
+   /* Displaying Infos */
+   allInfos.forEach((item) => {
+      item.classList.remove('hidden');
+   });
 
-  // Trigger The Main Function //
+   // Trigger The Main Function //
 
-  main(choiceSelected);
+   main(choiceSelected);
 });
 
 /**
@@ -53,44 +58,47 @@ cityView.addEventListener("change", e => {
  * Declare main Function
  */
 
-let main = choice => {
-  const yearsResult = data.map(x => x.fields.annee + "</br>"),
-    unemploymentResult = data.map(x => x.fields[choice] + "%</br>");
+let main = (choice) => {
+   const yearsResult = data.map((x) => x.fields.annee + '</br>');
 
-  /**
-   * Retrieving MIN AND MAX Values
-   */
+   const unemploymentResult = data.map((x) => x.fields[choice] + '%</br>');
 
-  const unemploymentResultParsed = data.map(x =>
-    x.fields[choice].replace(",", ".")
-  );
-  const min = Math.min(...unemploymentResultParsed),
-    max = Math.max(...unemploymentResultParsed);
+   /**
+    * Retrieving MIN AND MAX Values
+    */
 
-  /**
-   * Join Array
-   */
+   const unemploymentResultParsed = data.map((x) =>
+      x.fields[choice].replace(',', '.')
+   );
+   const min = Math.min(...unemploymentResultParsed);
 
-  const yearsWellSplited = yearsResult.join(" "),
-    unemploymentWellSplited = unemploymentResult.join(" ");
+   const max = Math.max(...unemploymentResultParsed);
 
-  /**
-   * Average
-   */
+   /**
+    * Join Array
+    */
 
-  const intArray = unemploymentResultParsed.map(Number);
+   const yearsWellSplited = yearsResult.join(' ');
 
-  const reducer = (accumulator, currentValue) =>
-    (accumulator + currentValue) / intArray.length;
-  const average = intArray.reduce(reducer) * 100;
+   const unemploymentWellSplited = unemploymentResult.join(' ');
 
-  /**
-   * Inject Data into the view
-   */
+   /**
+    * Average
+    */
 
-  yearsView.innerHTML = yearsWellSplited;
-  unemploymentView.innerHTML = unemploymentWellSplited;
-  maxView.innerHTML = max + "%";
-  minView.innerHTML = min + "%";
-  averageView.innerHTML = average.toFixed(1) + "%";
+   const intArray = unemploymentResultParsed.map(Number);
+
+   const reducer = (accumulator, currentValue) =>
+      (accumulator + currentValue) / intArray.length;
+   const average = intArray.reduce(reducer) * 100;
+
+   /**
+    * Inject Data into the view
+    */
+
+   yearsView.innerHTML = yearsWellSplited;
+   unemploymentView.innerHTML = unemploymentWellSplited;
+   maxView.innerHTML = max + '%';
+   minView.innerHTML = min + '%';
+   averageView.innerHTML = average.toFixed(1) + '%';
 };
